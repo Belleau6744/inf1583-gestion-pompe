f594c81dad70479bd6f96888b5e21a34ce8cb9bc
Jérémy Belleau <belleauj22@gmail.com>
2024-10-31 17:33:13 -0400

    update login page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,6 +1,10 @@
 import { Features } from "@features";
-import { Button, Card, Divider, TextField, Typography } from "@mui/material";
-import { ChangeEvent, useState } from "react";
+import LockIcon from '@mui/icons-material/Lock';
+import PersonIcon from '@mui/icons-material/Person';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { Button, Card, Divider, IconButton, TextField, Typography } from "@mui/material";
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
@@ -14,9 +18,12 @@ const Container = styled.div`
 const HomePage = () => {
     const dispatch = useDispatch();
     const nav = useNavigate();
+    const inputRef = useRef<HTMLFormElement>(null);
     const [ username, setUsername ] = useState<string>("");
     const [ password, setPassword ] = useState<string>("");
+    const [showPassword, setShowPassword] = useState(false);
 
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
 
     const handleOnChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
         const { value } = e.target;
@@ -28,7 +35,9 @@ const HomePage = () => {
         setUsername(value);
     }
 
-    const handleLogin = () => {
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        event.stopPropagation();
         if (username === ADMIN_USER.username && password === ADMIN_USER.password) {
             dispatch(Features.UserFeature.action.setUserAuthStatus(true));
             nav("/pompes");
@@ -40,11 +49,32 @@ const HomePage = () => {
             <Card sx={{ position: 'absolute', top: "50%", left: "50%", transform: "translate(-50%, -50%)", padding: "25px", width: "30%", height: "auto" }} elevation={24}>
                 <Typography variant="h3">Bienvenue</Typography>
                 <Divider />
-                <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', gap: '25px', marginTop: "20px" }}>
-                    <TextField label="Nom d'utilisateur" onChange={handleOnChangeUsername} value={username}></TextField>
-                    <TextField label="Mot de passe" type="password" onChange={handleOnChangePassword} value={password}/>
-                    <Button variant="contained" onClick={handleLogin}>Se connecter</Button>
-                </div>
+                <form ref={inputRef} onSubmit={handleSubmit} style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', gap: '25px', marginTop: "20px" }}>
+                    <TextField 
+                        slotProps={{
+                            input: {
+                                startAdornment: <PersonIcon /> 
+                            }
+                        }}
+                        required
+                        label="Nom d'utilisateur"
+                        onChange={handleOnChangeUsername}
+                        value={username}></TextField>
+
+                    <TextField
+                        slotProps={{
+                            input: {
+                                endAdornment: <IconButton onClick={handleClickShowPassword}>{showPassword ? <VisibilityOff /> : <Visibility />}</IconButton>, startAdornment: <LockIcon />
+                            }
+                        }}
+                        required
+                        label="Mot de passe"
+                        type={showPassword ? 'text' : 'password'}
+                        onChange={handleOnChangePassword}
+                        value={password}/>
+                        
+                    <Button type="submit" variant="contained">Se connecter</Button>
+                </form>
             </Card>
         </Container>       
     )
